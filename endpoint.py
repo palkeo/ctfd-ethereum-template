d7f436901d32788981e7e86fb0960de202e6d3bc
palkeo <contact@palkeo.com>
2019-01-25 18:22:17 +0100

    It should work now. Not 100% tested because my node is unhappy.

--- endpoint.py
+++ endpoint.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 from flask import Flask, request, abort
 from web3.auto import w3
+import web3
 
 import logging
 import queue
+import random
 import threading
 
 app = Flask(__name__)
-
-logger = app.logger
+logger = logging.getLogger(__name__)
 
 # Constructor bytecode. You could also compile solidity code from python:
 # https://web3py.readthedocs.io/en/stable/contracts.html
 CONTRACT_BYTECODE = "608060405234801561001057600080fd5b5060c58061001f6000396000f3fe6080604052348015600f57600080fd5b50600436106045576000357c0100000000000000000000000000000000000000000000000000000000900480639d11877014604a575b600080fd5b607360048036036020811015605e57600080fd5b81019080803590602001909291905050506075565b005b602a8114156096573373ffffffffffffffffffffffffffffffffffffffff16ff5b5056fea165627a7a7230582074abcc958736d80480501f965d4df8a204d5304da8606efd7081638dc9af85ed0029"
 CONTRACT_ABI = [{"constant":False,"inputs":[{"name":"secret","type":"uint256"}],"name":"destroy","outputs":[],"payable":False,"stateMutability":"nonpayable","type":"function"}]
+
+MIN_DUMMY_CONTRACTS = 10
+MAX_DUMMY_CONTRACTS = 30
 
 # TODO: Maybe put that dict into a JSON file on disk? So it's persisted in
 # case we need to restart the server.
@@ -80,10 +84,19 @@ def check_solve():
 
 
 def create_contract():
-    logger.info("Creating new contract...")
+    logger.info("Creating new contract... Last block is %i", w3.eth.blockNumber)
+
+    try:
+        from_addr = w3.personal.listAccounts[0]
+    except IndexError:
+        raise RuntimeError("Cannot find an account. Make sure you have one with some ethers to use to create contracts.")
+    logger.info("From address: %s", from_addr)
+
+    if not w3.personal.unlockAccount(from_addr, ""):  # No password.
+        raise RuntimeError("Unable to unlock your account. Make sure it has no passphrase.")
 
     Contract = w3.eth.contract(abi=CONTRACT_ABI, bytecode=CONTRACT_BYTECODE)
-    tx_hash = Contract.constructor().transact()
+    tx_hash = Contract.constructor().transact({'from': from_addr})
 
     # Wait for the transaction to be mined.
     tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
@@ -95,11 +108,17 @@ def create_contract():
 
 def contract_creator():
     while True:
+        # Drown the actual contracts that are used in some noise, so it's not
+        # obvious to pivot to other teams.
+        for i in range(random.randint(MIN_DUMMY_CONTRACTS, MAX_DUMMY_CONTRACTS)):
+            create_contract()
         addr_queue.put(create_contract())
 
 
 if __name__ == "__main__":
-    logger.setLevel(logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
+    # Let's create one initially, so we make sure it works.
+    create_contract()
     contract_creator_thread = threading.Thread(target=contract_creator)
     contract_creator_thread.start()
     app.run(debug=True, threaded=True, host="127.0.0.1", port=4001)
